602f1db41f59108d2c304ee5a91bdc6dd8605ff9
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2022-07-23 20:10:56 +0300

    Верстка. Начало работы

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import "./index.scss";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+	<React.StrictMode>
+		<CssBaseline />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
+);
