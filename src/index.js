899ee3bc67f7f1d1051efa755e5049ce9079e67e
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2022-07-23 20:47:22 +0300

    Базовая настройка redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -7,6 +8,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -14,7 +16,11 @@ root.render(
 	<React.StrictMode>
 		<CssBaseline />
 		<ThemeProvider theme={theme}>
-			<App />
+			<BrowserRouter>
+				<Provider store={store}>
+					<App />
+				</Provider>
+			</BrowserRouter>
 		</ThemeProvider>
 	</React.StrictMode>
 );
