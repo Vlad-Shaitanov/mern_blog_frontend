94efba0cd10729c07b481130d4cd86532f0ac72f
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2022-07-31 14:10:45 +0300

    Получение всех постов

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-export const instance = axios.create({
-	baseURL: 'https://localhost:3010',
-	timeout: 1000,
+const instance = axios.create({
+	baseURL: " http://localhost:3010",
+
 	headers: {
-		"Content- Type": "application/json"
+		"Content-Type": "application/json"
 	}
 });
+
+export default instance;
